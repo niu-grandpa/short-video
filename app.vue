71a553a3487eb5ffee2c33ce047ce1ca7caebd38
niu-grandpa <2864103063@qq.com>
2023-08-17 11:03:21 +0800

    自动登录逻辑

--- app.vue
+++ app.vue
@@ -10,16 +10,20 @@ import { message } from 'ant-design-vue';
 
 const { $generalStore, $userStore, $profileStore } = useNuxtApp();
 
+const { token, uid } = toRefs($userStore);
+
 // 检查session是否过期
 onMounted(async () => {
-  if ($userStore.token) {
-    const res = await $generalStore.hasSessionExpired();
-    if (res) {
+  if (token) {
+    const expired = await $generalStore.hasSessionExpired();
+    if (expired) {
       message.info('登录已过期');
       $generalStore.restData();
       $userStore.restData();
       $profileStore.restData();
       $generalStore.isLoginOpen = true;
+    } else if (!uid) {
+      await $userStore.login(token);
     }
   }
 });
