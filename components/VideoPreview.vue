ce7f6f0cfdcfe62ef083306d53e602b5e9160f8c
niu-grandpa <2864103063@qq.com>
2023-08-21 21:59:36 +0800

    对接视频接口，优化逻辑

--- components/VideoPreview.vue
+++ components/VideoPreview.vue
@@ -1,94 +1,128 @@
 <template>
-  <section
-    @contextmenu.prevent=""
-    @mouseenter="() => onVideoHover(true)"
-    @mouseleave="() => onVideoHover(false)"
-    class="relative w-[260px] h-[480px] min-h-[480px] max-h-[580px] max-w-[260px] bg-black rounded-xl">
-    <section class="absolute w-full top-[12px] px-[6px] text-white z-20">
-      <ARow justify="center">
-        <ACol :span="5">
-          <NuxtLink to="/profile/1">
-            <AAvatar :size="42" :src="UserIcon" class="border-3 border-white" />
-          </NuxtLink>
-        </ACol>
+  <ASpin :spinning="spinning" :delay="800">
+    <section
+      @contextmenu.prevent=""
+      @mouseenter="() => onVideoHover(true)"
+      @mouseleave="() => onVideoHover(false)"
+      class="relative w-[260px] h-[480px] min-h-[480px] max-h-[580px] max-w-[260px] bg-black rounded-xl">
+      <section class="absolute w-full top-[12px] px-[6px] text-white z-20">
+        <ARow justify="center">
+          <ACol :span="5">
+            <NuxtLink to="/profile/1">
+              <AAvatar
+                :size="42"
+                :src="data.avatar"
+                class="border-3 border-white" />
+            </NuxtLink>
+          </ACol>
 
-        <ACol :span="18">
-          <section class="font-extrabold">
-            <p
-              class="inline-block max-w-100px w-[100px] overflow-hidden whitespace-nowrap overflow-ellipsis">
-              用户名用户名用户名
-            </p>
-            <button
-              @click="onFollow"
-              :class="isFollow ? 'bg-gray-500' : ''"
-              class="ml-[8px] py-[2px] px-[8px] rounded-xl bg-[#ff4d4f] text-[10px]">
-              <span class="relative top-[-3px]">
-                <PlusOutlined v-if="!isFollow" />
-                <CheckOutlined v-else />
-              </span>
-              {{ !isFollow ? '' : '已' }}关注
-            </button>
-          </section>
+          <ACol :span="18">
+            <section class="font-extrabold">
+              <p
+                class="inline-block max-w-100px w-[100px] overflow-hidden whitespace-nowrap overflow-ellipsis">
+                {{ data.author }}
+              </p>
+              <button
+                @click="onFollow"
+                :class="isFollow ? 'bg-gray-500' : ''"
+                class="ml-[8px] py-[2px] px-[8px] rounded-xl bg-[#ff4d4f] text-[10px]">
+                <span class="mr-[4px]">
+                  <PlusOutlined v-if="!isFollow" />
+                  <CheckOutlined v-else />
+                </span>
+                <span>{{ !isFollow ? '' : '已' }}关注</span>
+              </button>
+            </section>
 
-          <small class="relative top-[-5px]">33粉丝</small>
-        </ACol>
-      </ARow>
+            <small class="relative top-[-5px]">{{ authorFollowers }}粉丝</small>
+          </ACol>
+        </ARow>
 
-      <div
-        class="pt-[4px] font-extrabold max-w-235px w-[235px] overflow-hidden whitespace-nowrap overflow-ellipsis">
-        <ClientOnly>
-          <ATooltip title="丁正新曲《你的香烟》">
-            <ATypographyText
-              :class="{ 'w-[150px]': ellipsis }"
-              class="text-white"
-              content="丁正新曲《你的香烟》" />
-          </ATooltip>
-        </ClientOnly>
-        1567 播放
-      </div>
+        <div
+          class="p-[4px] max-w-235px w-[235px] overflow-hidden whitespace-nowrap overflow-ellipsis">
+          <ATypographyText
+            strong
+            :class="{ 'w-[150px]': ellipsis }"
+            class="text-white"
+            :content="data.title" />
+          <small class="block">{{ data.watched }} 播放</small>
+        </div>
+      </section>
+
+      <video
+        loop
+        muted
+        ref="videoRef"
+        :src="data.url"
+        @click="() => $router.push(`/post/${data._id}`)"
+        class="object-cover mx-auto h-full cursor-pointer rounded-xl" />
+
+      <VideoActionBar
+        :postId="data._id!"
+        :likes="data.likes.length"
+        :comments="0"
+        :star="data.favorites.length" />
+
+      <PlayCircleOutlined
+        v-show="!isHover"
+        class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[3.5rem] text-white" />
     </section>
-
-    <video
-      loop
-      muted
-      ref="videoRef"
-      @click="onToPost"
-      src="~/public/video.mp4"
-      class="object-cover mx-auto h-full cursor-pointer rounded-xl" />
-
-    <VideoActionBar :postId="1" :likes="0" :comments="0" :star="0" />
-
-    <PlayCircleOutlined
-      v-if="!isHover"
-      class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[3.5rem] text-white" />
-  </section>
+  </ASpin>
 </template>
 
 <script setup lang="ts">
-import UserIcon from '@/assets/images/user-placeholder.jpg';
 import { useASDCallback, useDebounce } from '@/hooks';
+import { message } from 'ant-design-vue';
+import { IVideo } from '~/services/types/video_api';
 
-// const props = defineProps<{
-//
-// }>();
+const { $userStore, $profileStore, $generalStore } = useNuxtApp();
 
-const router = useRouter();
+const props = defineProps<{ dataSource: object }>();
 
+const data = ref<IVideo>(props.dataSource as IVideo);
 const videoRef = ref<HTMLVideoElement | null>(null);
-const isFollow = ref(false);
+const isFollow = ref($profileStore.following.includes(data.value.uid));
+
 const isHover = ref(false);
 const ellipsis = ref(true);
+const spinning = ref(true);
+const authorFollowers = ref(0);
+
+onMounted(async () => {
+  try {
+    const { following } = await $profileStore.getProfile(data.value.uid);
+    authorFollowers.value = following.length;
+  } catch (error) {
+    console.warn('user not found');
+  }
+});
+
+onMounted(() => {
+  if (videoRef.value) {
+    videoRef.value.addEventListener(
+      'loadeddata',
+      () => (spinning.value = false)
+    );
+  }
+});
 
 const onVideoHover = useDebounce((flag: boolean) => {
   isHover.value = flag;
-  videoRef.value?.[flag ? 'play' : 'pause']();
-}, 300);
+  videoRef.value![flag ? 'play' : 'pause']();
+}, 500);
 
-const onFollow = useASDCallback(() => {
-  isFollow.value = !isFollow.value;
-});
-
-const onToPost = useASDCallback(() => {
-  router.push(`/post/${1}`);
+const onFollow = useASDCallback(async () => {
+  try {
+    const flag = !isFollow.value;
+    await $generalStore.following({
+      uid: $userStore.uid,
+      someone: data.value.uid,
+      flag,
+    });
+    isFollow.value = flag;
+    flag && message.success('关注成功');
+  } catch (error) {
+    message.error('关注失败');
+  }
 });
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,65 @@
   <Head>
     <title>short-video 首页</title>
   </Head>
-  <SrcollLoading @load-more="onLoadMore">
-    <MainLayout>
-      <ARow class="my-[18px]" :gutter="[48, 48]" style="margin-right: 0">
-        <ACol :span="6"><VideoPreview /></ACol>
-      </ARow>
-    </MainLayout>
-  </SrcollLoading>
+
+  <MainLayout
+    ref="containerRef"
+    class="height-[100vh] overflow-y-auto overflow-x-hidden">
+    <AAffix :offset-top="56">
+      <AButton
+        v-if="videos.length"
+        type="primary"
+        :loading="loading"
+        @click="onChange"
+        class="relative left-[90%] top-[16px]">
+        <template #icon><ReloadOutlined /></template>
+        换一批
+      </AButton>
+    </AAffix>
+
+    <ARow v-if="videos.length" :gutter="[48, 64]" class="py-[42px]">
+      <ACol :span="6" v-for="item in videos" :key="item._id">
+        <VideoPreview :data-source="item" />
+      </ACol>
+    </ARow>
+
+    <AEmpty
+      v-else
+      :image="simpleImage"
+      description="暂无数据"
+      class="pt-[160px]" />
+  </MainLayout>
 </template>
 
 <script setup lang="ts">
+import { Empty, message } from 'ant-design-vue';
+import { IVideo } from 'services/types/video_api';
 import { MainLayout } from '~/layouts';
 
 const { $generalStore } = useNuxtApp();
 
-const onLoadMore = (data: any) => {
-  console.log('loaded more: ', data);
+const simpleImage = Empty.PRESENTED_IMAGE_SIMPLE;
+
+const videos = ref<IVideo[]>([]);
+const loading = ref(false);
+
+const getVideoData = async () => {
+  try {
+    loading.value = true;
+    const data = await $generalStore.getRandomVideo(8);
+    videos.value = data;
+  } catch (error) {
+    message.error('请求数据失败');
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
+};
+
+onMounted(getVideoData);
+
+const onChange = async () => {
+  window.scrollTo(0, 0);
+  await getVideoData();
 };
 </script>
-
-<style>
-body {
-  overflow: hidden;
-}
-</style>
