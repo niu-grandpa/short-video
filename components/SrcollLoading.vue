b93f7b606c12be64ca251745b5f7daa6a9c399ff
niu-grandpa <2864103063@qq.com>
2023-08-21 21:59:08 +0800

    添加props和ts类型

--- components/SrcollLoading.vue
+++ components/SrcollLoading.vue
@@ -1,19 +1,35 @@
 <template>
-  <section ref="container">
+  <div ref="container" class="h-[100vh] overflow-y-auto">
     <slot />
-  </section>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useSrcollLoading } from '~/hooks';
 
+interface Props {
+  delay: number;
+  loop: boolean;
+  immediately: boolean;
+  request: {
+    url?: string;
+    page: number;
+    size: number;
+    sort?: -1 | 1;
+    onlyFromList?: unknown[];
+    error?: () => void;
+  };
+}
+
 const emit = defineEmits(['loadMore']);
-const props = defineProps<{}>();
+
+const props = defineProps<Partial<Props>>();
 
 const container = ref<HTMLElement | null>(null);
 
 onMounted(() => {
   useSrcollLoading(container.value!, data => emit('loadMore', data), {
+    ...props,
     immediately: true,
   });
 });
