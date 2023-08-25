9695b3cebb505361404a70b8385ebc798c369573
niu-grandpa <2864103063@qq.com>
2023-08-25 13:44:05 +0800

    调整权限判断，在登录的情况下也要对token有效性进行验证

--- hooks/useASDCallback.ts
+++ hooks/useASDCallback.ts
@@ -1,16 +1,32 @@
 import { useGeneralStore } from '~/stores/general';
 import { useUserStore } from '~/stores/user';
 
-export const useASDCallback = (fn: () => any, to = '') => {
+export const useASDCallback = (fn: (...args: any[]) => any, to = '') => {
   const router = useRouter();
+  const userStore = useUserStore();
+  const generalStore = useGeneralStore();
 
-  if (!useUserStore().uid) {
+  const rest = () => {
+    generalStore.restAll();
+    generalStore.isLoginOpen = true;
+  };
+
+  if (!userStore.uid) {
     return () => {
-      if (to !== '') {
-        router.push(to);
-      }
-      useGeneralStore().isLoginOpen = true;
+      if (to !== '') router.push(to);
+      rest();
+      console.log('用户未登录');
     };
+  } else if (userStore.token) {
+    let isExpired = false;
+    userStore.hasSessionExpired().then(res => (isExpired = res));
+    if (isExpired) {
+      return () => {
+        rest();
+        console.log('登录会话已过期');
+      };
+    }
   }
+
   return fn;
 };
