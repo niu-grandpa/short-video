6cb4ed57a4f531f7d48d8aa105da58cfad2e2eb5
niu-grandpa <2864103063@qq.com>
2023-08-25 13:42:06 +0800

    重构评论模块代码、联调评论接口

--- components/VideoComments.vue
+++ components/VideoComments.vue
@@ -1,55 +1,132 @@
 <template>
-  <template
-    v-if="$props.dataSource.length"
-    v-for="parent in $props.dataSource"
-    :key="parent._id">
-    <VideoCommentItem
-      :_id="parent._id"
-      :author="parent.author"
-      :avatar="parent.avatar"
-      :content="parent.content"
-      :likes="parent.likes"
-      :dislikes="parent.dislikes"
-      :parentId="parent.parentId"
-      :datetime="parent.datetime"
-      @reply="res => $emit('reply', res)">
-      <template v-if="!$props.loadSub">
-        <a
-          v-if="parent.reply?.length"
-          class="text-[#1677ff]"
-          @click="() => $emit('loadSubReply', parent._id)">
-          {{ parent.reply?.length }} 条回复
-        </a>
-      </template>
-      <template v-else v-for="sub in parent.reply" :key="sub._id">
+  <SrcollLoading
+    :request="{ url }"
+    :reload="reload"
+    class="px-[18px]"
+    :style="{ maxHeight: props.maxHeight }"
+    @load-more="onLoadComments">
+    <template v-if="props.isSecondary && fatherComment">
+      <VideoCommentItem
+        :data-source="fatherComment"
+        @reply="() => onGetReplyData(fatherComment)">
         <VideoCommentItem
-          :_id="sub._id"
-          :author="sub.author"
-          :avatar="sub.avatar"
-          :content="sub.content"
-          :likes="sub.likes"
-          :dislikes="sub.dislikes"
-          :parentId="sub.parentId"
-          :datetime="sub.datetime"
-          @reply="res => $emit('reply', res)" />
-      </template>
-    </VideoCommentItem>
-  </template>
+          v-for="child in comments.slice(0, comments.length - 1)"
+          :key="child.cid"
+          :data-source="child"
+          @reply="() => onGetReplyData(child)" />
+      </VideoCommentItem>
+    </template>
 
-  <AEmpty v-else description="暂无评论" />
+    <template v-else>
+      <VideoCommentItem
+        v-if="comments.length"
+        v-for="item in comments"
+        :key="item.cid"
+        :data-source="item"
+        @reply="() => onGetReplyData(item)">
+        <a
+          v-if="item.replies"
+          class="text-[#1677ff]"
+          @click="() => $emit('get-secondary', item.cid)">
+          共{{ item.replies }}条回复
+        </a>
+      </VideoCommentItem>
+      <AEmpty v-else description="暂无评论" />
+    </template>
+  </SrcollLoading>
+
+  <VideoCommentsPost
+    :loading="loading"
+    :placeholder="placeholder"
+    @post="onPostComment" />
 </template>
 
 <script setup lang="ts">
-import { type CommentData, type CommentItemType } from './VideoCommentItem.vue';
+import { message } from 'ant-design-vue';
+import { useASDCallback } from '~/hooks';
+import { CommentLevel, IComment } from '~/services/types/comment_api';
 
-export type CommentsType = CommentItemType & {
-  reply?: CommentItemType[];
+export type ReplyTo = {
+  belong: string;
+  level: CommentLevel;
+  content: string;
+};
+
+type Props = {
+  belong: string;
+  maxHeight?: string | number;
+  isSecondary?: boolean;
+  level: CommentLevel;
 };
 
 defineEmits<{
-  (e: 'reply', res: CommentData): void;
-  (e: 'loadSubReply', id: string): void;
+  (e: 'get-secondary', id: string): void;
 }>();
 
-defineProps<{ dataSource: CommentsType[]; loadSub?: boolean }>();
+const {
+  $userStore: { uid },
+  $generalStore: { postComment },
+} = useNuxtApp();
+
+const props = defineProps<Props>();
+
+const url = computed(
+  () => `/comments/list?belong=${props.belong}&level=${props.level}`
+);
+
+const loading = ref(false);
+const reload = ref(false);
+
+const comments = ref<IComment[]>([]);
+const placeholder = ref('添加评论...');
+
+const curBelong = ref(props.belong);
+const curLevel = ref(props.level);
+
+const fatherComment = computed(() => comments.value[comments.value.length - 1]);
+
+const restData = () => {
+  placeholder.value = '添加评论...';
+  curBelong.value = props.belong;
+  curLevel.value = props.level;
+};
+
+const onLoadComments = (res: IComment[]) => {
+  if (reload.value) {
+    comments.value.length = 0;
+  }
+  comments.value.push(...res);
+};
+
+// 点击评论回复
+const onGetReplyData = ({ cid, author }: IComment) => {
+  curBelong.value = cid;
+  curLevel.value = CommentLevel.TOW; // 只要是回复的，层级都属于2
+  placeholder.value = `回复 @${author}: `;
+};
+
+const onPostComment = useASDCallback(async (content: string) => {
+  if (!content) {
+    message.info('不能发送空白评论!');
+    return;
+  }
+  loading.value = true;
+  try {
+    await postComment({
+      uid,
+      content,
+      level: curLevel.value,
+      belong: curBelong.value,
+    });
+    reload.value = true;
+    message.success('评论成功');
+    restData();
+  } catch (error) {
+    message.error('评论发送失败');
+    console.log(error);
+  } finally {
+    loading.value = false;
+    setTimeout(() => (reload.value = false), 200);
+  }
+});
 </script>
--- components/VideoCommentsPost.vue
+++ components/VideoCommentsPost.vue
@@ -1,16 +1,26 @@
 <template>
   <ARow class="px-[8px] border-t border-solid" align="middle">
     <ACol :span="3">
-      <AAvatar size="large" />
+      <Avatar />
     </ACol>
     <ACol :span="21">
       <AInputGroup compact class="mt-[8px]">
         <ATextarea
+          autofocus
           v-model:value="content"
-          :autosize="{ minRows: 3, maxRows: 3 }"
+          :autoSize="{ minRows: 3, maxRows: 3 }"
           :placeholder="$props.placeholder || '添加评论...'"
           style="width: calc(100% - 65px)" />
-        <AButton type="primary" class="h-[75px]" @click="onPostComment">
+        <AButton
+          type="primary"
+          class="h-[75px]"
+          :loading="$props.loading"
+          @click="
+            () => {
+              $emit('post', content);
+              content = '';
+            }
+          ">
           发布
         </AButton>
       </AInputGroup>
@@ -18,41 +28,8 @@
   </ARow>
 </template>
 
-<script setup lang="ts">
-import { message } from 'ant-design-vue';
-import { type CommentData } from './VideoCommentItem.vue';
-
-export type ReplyData = {
-  postId: string;
-  isReplySub: boolean;
-  commentObj?: CommentData;
-};
-
-defineProps<{ placeholder: string }>();
-
-const { $generalStore } = useNuxtApp();
-
-const emit = defineEmits<{
-  (e: 'post'): void;
-}>();
-
+<script setup>
+defineEmits(['post']);
+defineProps(['placeholder', 'loading']);
 const content = ref('');
-
-const onPostComment = async () => {
-  if (!content.value) {
-    message.info('不能发送空白评论!');
-    return;
-  }
-
-  emit('post');
-
-  const { postId, isReplySub, commentObj } = $generalStore.replyData;
-  // 增加新评论
-  if (!isReplySub && !commentObj) {
-  } else {
-    // 回复某条评论
-  }
-
-  content.value = '';
-};
 </script>
