e9ac9f95320e0404f0aadf38302595f271a13f81
niu-grandpa <2864103063@qq.com>
2023-08-25 13:40:51 +0800

    更新props，增加reload功能

--- components/SrcollLoading.vue
+++ components/SrcollLoading.vue
@@ -9,15 +9,12 @@ import { useSrcollLoading } from '~/hooks';
 
 interface Props {
   delay: number;
+  reload?: boolean;
   loop: boolean;
   immediately: boolean;
   request: {
-    url?: string;
-    page: number;
-    size: number;
+    url: string;
     sort?: -1 | 1;
-    onlyFromList?: unknown[];
-    error?: () => void;
   };
 }
 
@@ -27,10 +24,25 @@ const props = defineProps<Partial<Props>>();
 
 const container = ref<HTMLElement | null>(null);
 
-onMounted(() => {
+const loadData = (url = props.request?.url) => {
   useSrcollLoading(container.value!, data => emit('loadMore', data), {
     ...props,
+    request: {
+      url,
+      page: 0,
+      size: 10,
+    },
     immediately: true,
   });
-});
+};
+
+onMounted(loadData);
+
+watch(
+  () => [props.reload, props.request?.url],
+  ([newVal1, newVal2]) => {
+    if (newVal1) loadData();
+    if (newVal2) loadData(newVal2 as string);
+  }
+);
 </script>
