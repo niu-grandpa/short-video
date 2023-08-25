05ccf1014b7e5432bf07b39526bfa940b2f46a61
niu-grandpa <2864103063@qq.com>
2023-08-25 13:46:26 +0800

    视频页重构、联调评论与视频接口

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -1,58 +1,45 @@
 <template>
   <Head>
-    <Title>视频详情_short video</Title>
+    <Title>{{ videoData?.title }}_short video</Title>
   </Head>
 
-  <ARow class="overflow-hidden">
-    <ACol :span="16" class="relative pl-[12px] py-[44px] bg-[black]">
+  <ARow>
+    <ACol :span="15" class="relative bg-[black] h-[100vh]">
       <ASpace
         direction="vertical"
-        class="absolute top-[64px] right-[34px] z-20">
-        <AButton
-          ghost
-          shape="circle"
-          :icon="h(UpOutlined)"
-          class="flex items-center justify-center" />
+        class="absolute top-[48px] right-[16px] z-20">
+        <AButton ghost shape="circle" :icon="h(UpOutlined)" @click="onSwitch" />
         <AButton
           ghost
           shape="circle"
           :icon="h(DownOutlined)"
-          class="flex items-center justify-center" />
+          @click="() => onSwitch(true)" />
       </ASpace>
-
-      <section class="absolute z-20 opacity-80 top-[8px] left-[12px]">
-        <ATypographyTitle :level="4" style="color: white">
-          这是视频标题
-        </ATypographyTitle>
-      </section>
 
       <video
         loop
         controls
-        autoplay
         ref="videoRef"
-        src="~/public/video.mp4"
-        class="object-cover w-full h-[90vh]" />
+        preload="metadata"
+        :src="videoData?.url"
+        class="w-full h-full" />
     </ACol>
 
-    <ACol :span="8" class="pt-[14px]" ref="commentsRef">
+    <ACol :span="9" class="pt-[14px]" ref="commentsRef">
       <APageHeader
-        title="评论"
-        class="py-[0] border-b"
-        :sub-title="commentLen"
-        @back="() => router.push($generalStore.backUrl)" />
+        :title="`${videoData?.title} - ${videoData?.author}`"
+        class="py-[0]"
+        @back="() => $router.push($generalStore.backUrl)" />
 
-      <section
-        class="overflow-auto h-[calc(100vh-148px)] max-h-[calc(100vh-148px)] px-[26px] pb-[24px]">
-        <ClientOnly>
-          <VideoComments
-            :data-source="comments"
-            @reply="onGetReplyObj"
-            @load-sub-reply="onLoadSubComments" />
-        </ClientOnly>
-      </section>
+      <ADivider class="px-[18px]" style="margin: 0; margin-top: 12px">
+        评论区
+      </ADivider>
 
-      <VideoCommentsPost :placeholder="placeholder" @post="onPostComment" />
+      <VideoComments
+        :belong="vid"
+        :level="CommentLevel.ONE"
+        max-height="calc(100vh - 188px)"
+        @get-secondary="onLoadSecondary" />
     </ACol>
   </ARow>
 
@@ -61,180 +48,103 @@
       title="回复"
       destroyOnClose
       :mask="false"
-      v-model:open="isLoadSubComments"
+      v-model:open="openDrawer"
       :keyboard="false"
-      :width="commentsRef?.['$el']['offsetWidth']">
-      // todo
+      :bodyStyle="{ padding: 0 }"
+      :width="drawerWidth">
+      <VideoComments
+        :belong="cpId"
+        is-secondary
+        :level="CommentLevel.TOW"
+        max-height="calc(100vh - 151px)" />
     </ADrawer>
   </ClientOnly>
 </template>
 
 <script setup lang="ts">
 import { DownOutlined, UpOutlined } from '@ant-design/icons-vue';
-import { type CommentData } from '~/components/VideoCommentItem.vue';
-import { type CommentsType } from '~/components/VideoComments.vue';
+import { message } from 'ant-design-vue';
+import { useAdjustVideoBuffering } from '~/hooks';
+import { CommentLevel } from '~/services/types/comment_api';
+import { IVideo } from '~/services/types/video_api';
 
-const { $generalStore, $userStore } = useNuxtApp();
+const {
+  $generalStore: { getRandomVideo, getOneVideo },
+} = useNuxtApp();
 
 const route = useRoute();
-const router = useRouter();
 
+const cpId = ref(''); // 隶属于子评论的父级id
+const vid = ref(route.params.id as string);
+
+const openDrawer = ref(false);
 const commentsRef = ref(null);
-const videoRef = ref<HTMLVideoElement | null>(null);
 
-const placeholder = ref('');
-const isLoadSubComments = ref(false);
-
-const commentObj = ref<CommentData>();
-const comments = ref<CommentsType[]>([
-  {
-    _id: 'string',
-    author: 'Jshon',
-    avatar: '',
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently. ',
-    likes: 164,
-    dislikes: 0,
-    datetime: 1691722553102,
-    reply: [
-      {
-        _id: 'string2',
-        parentId: 'string',
-        author: 'Jshon2',
-        avatar: '',
-        content: 'We supply a series of design principles',
-        likes: 164,
-        dislikes: 0,
-        datetime: 1691722553102,
-      },
-      {
-        _id: 'string3',
-        parentId: 'string',
-        author: 'Jshon2',
-        avatar: '',
-        content: 'We supply a series of design principles',
-        likes: 164,
-        dislikes: 0,
-        datetime: 1691722553102,
-      },
-      {
-        _id: 'string4',
-        parentId: 'string',
-        author: 'Jshon2',
-        avatar: '',
-        content: 'We supply a series of design principles',
-        likes: 164,
-        dislikes: 0,
-        datetime: 1691722553102,
-      },
-      {
-        _id: 'string5',
-        parentId: 'string',
-        author: 'Jshon2',
-        avatar: '',
-        content: 'We supply a series of design principles',
-        likes: 164,
-        dislikes: 0,
-        datetime: 1691722553102,
-      },
-      {
-        _id: 'string6',
-        parentId: 'string',
-        author: 'Jshon2',
-        avatar: '',
-        content: 'We supply a series of design principles',
-        likes: 164,
-        dislikes: 0,
-        datetime: 1691722553102,
-      },
-      {
-        _id: 'string7',
-        parentId: 'string',
-        author: 'Jshon2',
-        avatar: '',
-        content: 'We supply a series of design principles',
-        likes: 164,
-        dislikes: 0,
-        datetime: 1691722553102,
-      },
-      {
-        _id: 'string8',
-        parentId: 'string',
-        author: 'Jshon2',
-        avatar: '',
-        content: 'We supply a series of design principles',
-        likes: 164,
-        dislikes: 0,
-        datetime: 1691722553102,
-      },
-      {
-        _id: 'string9',
-        parentId: 'string',
-        author: 'Jshon2',
-        avatar: '',
-        content: 'We supply a series of design principles',
-        likes: 164,
-        dislikes: 0,
-        datetime: 1691722553102,
-      },
-      {
-        _id: 'string10',
-        parentId: 'string',
-        author: 'Jshon2',
-        avatar: '',
-        content: 'We supply a series of design principles',
-        likes: 164,
-        dislikes: 0,
-        datetime: 1691722553102,
-      },
-      {
-        _id: 'string11',
-        parentId: 'string',
-        author: 'Jshon2',
-        avatar: '',
-        content: 'We supply a series of design principles',
-        likes: 164,
-        dislikes: 0,
-        datetime: 1691722553102,
-      },
-    ],
-  },
-]);
-const currenComment = ref<CommentsType[]>([]);
-
-const commentLen = computed(() => {
-  const list = comments.value;
-  return list.length + list.reduce((p, c) => p + (c.reply?.length ?? 0), 0);
-});
-
-watch(
-  () => isLoadSubComments.value,
-  newVal => {
-    if (!newVal) {
-      placeholder.value = '';
-      commentObj.value = undefined;
-    }
-  }
+const drawerWidth = computed(
+  () => commentsRef.value?.['$el']['offsetWidth']! - 54
 );
 
-// 获取要回复的父评论数据
-const onGetReplyObj = (res: CommentData) => {
-  commentObj.value = res;
-  placeholder.value = `回复 @${res.author}: `;
+const videoArr = ref<IVideo[]>([]);
+const videoData = ref<IVideo>();
+
+const videoRef = ref<HTMLVideoElement | null>(null);
+
+const adjustBuffering = useAdjustVideoBuffering(videoRef.value!);
+
+onMounted(async () => {
+  try {
+    videoData.value = await getOneVideo(vid.value);
+  } catch (error) {
+    message.error('视频不存在');
+  }
+});
+
+onMounted(() => {
+  const velm = videoRef.value;
+  if (velm) {
+    velm.onloadeddata = () => {
+      velm.play();
+      adjustBuffering();
+      console.log('视频加载完成');
+    };
+    velm.load();
+  }
+});
+
+const onSwitch = async (next = false) => {
+  // 数组缓存优化
+  const arr = videoArr.value;
+  const curIdx = 0;
+  try {
+    if (next) {
+      const lastIdx = curIdx + 1;
+      // 如果找不到栈中的下一个视频，说明是在末尾位置需要请求新视频并入栈
+      if (!arr[lastIdx]) {
+        const data = (await getRandomVideo(1))[0];
+        arr.push(data);
+      }
+      // 在当前位置找到下一个
+      videoData.value = arr[lastIdx];
+    } else {
+      const prevIdx = curIdx - 1;
+      // 如果找不到栈中的上一个视频，说明是在首位需要请求新视频并压入首部
+      if (!arr[prevIdx]) {
+        const data = (await getRandomVideo(1))[0];
+        arr.unshift(data);
+      }
+      // 在当前位置找到上一个
+      videoData.value = arr[prevIdx];
+    }
+    vid.value = videoData.value.vid;
+  } catch (error) {
+    message.error('切换视频失败');
+    console.log(error);
+  }
 };
 
-// 加载子评论列表
-const onLoadSubComments = (id: string) => {
-  isLoadSubComments.value = true;
-  currenComment.value = comments.value.filter(({ _id }) => id === _id);
-};
-
-const onPostComment = () => {
-  $userStore.setReplyData({
-    postId: route.params.id as string,
-    isReplySub: isLoadSubComments.value,
-    commentObj: commentObj.value,
-  });
-  commentObj.value = undefined;
+// 加载子评论
+const onLoadSecondary = (pId: string) => {
+  cpId.value = pId;
+  openDrawer.value = true;
 };
 </script>
