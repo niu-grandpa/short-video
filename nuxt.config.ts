ae864a241ac3187d545e167629640a632838874f
niu-grandpa <2864103063@qq.com>
2023-08-30 23:55:04 +0800

    构建优化配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
+import { visualizer } from 'rollup-plugin-visualizer';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
+import viteCompression from 'vite-plugin-compression';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   pages: true,
+  imports: {
+    autoImport: true,
+  },
   modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
   css: ['~/assets/css/main.css'],
   postcss: {
@@ -13,8 +18,8 @@ export default defineNuxtConfig({
     },
   },
   vite: {
-    // 按需加载antd，提高编译速度、打包后的体积
     plugins: [
+      visualizer({ open: true }),
       Components({
         resolvers: [
           AntDesignVueResolver({
@@ -39,6 +44,39 @@ export default defineNuxtConfig({
         'ant-design-vue',
         '@ant-design/icons-vue',
       ],
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              // 让每个插件都打包成独立的文件
+              return id
+                .toString()
+                .split('node_modules/')[1]
+                .split('/')[0]
+                .toString();
+            }
+          },
+        },
+        plugins: [
+          viteCompression({
+            verbose: true, // 是否在控制台中输出压缩结果
+            disable: false,
+            threshold: 10240, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+            algorithm: 'gzip', // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
+            ext: '.gz',
+            deleteOriginFile: false, // 源文件压缩后是否删除
+          }),
+        ],
+      },
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
     },
   },
 });
